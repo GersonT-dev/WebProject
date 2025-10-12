098566287e4e6bce5c49cabee93a31b08b19bacb
GersonT-dev <Turciosg@cua.edu>
2025-10-12 09:51:16 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+// JavaScript source code
+console.log("Hello World!" + `    this 
+
+
+id  so fun ${34}`);
+
+console.log(3 / 2);
+
+if (true) {
+    let x = 6;
+    const y = 8;
+    var z = 8;
+    console.log(x + y + z);
+
+    console.log("This is true");
+}
